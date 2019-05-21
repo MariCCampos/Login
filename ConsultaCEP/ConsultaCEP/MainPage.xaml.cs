b6f1c03b0445a2075857b93c8c20024d4868fa18
MariCCampos <mccampos@server01.foursys.local>
2019-05-21 18:39:56 -0300

    validacao de login

--- ConsultaCEP/ConsultaCEP/MainPage.xaml.cs
+++ ConsultaCEP/ConsultaCEP/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsultaCEP.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,9 +10,45 @@ namespace ConsultaCEP
 {
     public partial class MainPage : ContentPage
     {
+        public List<Usuarios> usuarios = new List<Usuarios>();]
+        public Usuarios mariana = new Usuarios();
+        public Usuarios anderson = new Usuarios();
+        public Usuarios bianca = new Usuarios();
+
+
         public MainPage()
         {
+            mariana.email = "mariana@";
+            mariana.senha = "123";
+            usuarios.Add(mariana);
+
+            anderson.email = "anderson@";
+            anderson.senha = "abc";
+            usuarios.Add(anderson);
+
+            bianca.email = "bianca@";
+            bianca.senha = "pato";
+            usuarios.Add(bianca);
+
+
             InitializeComponent();
         }
+
+               
+        public void Logar( object obj, EventArgs args)
+        {
+            
+            if(email.Text != null && senha.Text != null && email.Text != "" && senha.Text != "")
+            {
+                App.Current.MainPage = new NavigationPage(new PageHome());
+            }
+            else
+            {
+                DisplayAlert("Alerta", "E-mail ou senha invalidos", "sair");
+            }
+
+        }
+
+        
     }
 }
---
+++ ConsultaCEP/ConsultaCEP/Model/Usuarios.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsultaCEP.Model
+{
+    public class Usuarios
+    {
+        public string email{get; set;}
+
+        public string senha {get; set;}
+    }
+}
